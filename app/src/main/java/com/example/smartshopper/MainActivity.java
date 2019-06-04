ab20235002361c02b239d120fffdbb0bb5336395
Lila06 <lisa_marie_assmann@web.de>
2019-06-04 22:47:34 +0200

    codescan not working

--- app/src/main/java/com/example/smartshopper/MainActivity.java
+++ app/src/main/java/com/example/smartshopper/MainActivity.java
@@ -1,13 +1,66 @@
 package com.example.smartshopper;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.smartshopper.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.cameraButton);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+                TextView txtView = (TextView) findViewById(R.id.textView);
+                Bitmap myBitmap = BitmapFactory.decodeResource(
+                        getApplicationContext().getResources(),
+                        R.drawable.puppy);
+                myImageView.setImageBitmap(myBitmap);
+
+                BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+                if(!detector.isOperational()){
+                    txtView.setText("Could not set up the detector!");
+                    return;
+                }
+
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Barcode> barcodes = detector.detect(frame);
+
+                Barcode thisCode = barcodes.valueAt(0);
+                txtView.setText(thisCode.rawValue);
+            }
+        });
+    }
+
+    //Called when the user taps the send button
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class); //intent provides runtime binding between separate components, e.g. two activities
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }

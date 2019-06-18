85e1bd106696c98a1d9ef3ff7ecf68ba7a52a52f
Lila06 <lisa_marie_assmann@web.de>
2019-06-18 10:43:51 +0200

    Barcode scanner: Camera view and display of scanned code

--- app/src/main/java/com/example/smartshopper/MainActivity.java
+++ app/src/main/java/com/example/smartshopper/MainActivity.java
@@ -1,64 +1,106 @@
 package com.example.smartshopper;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.SparseArray;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Camera;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.smartshopper.MESSAGE";
+
+
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 200;
+    private static final int CAMERA_REQUEST = 101;
+    private TextView textView;
+    private BarcodeDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.cameraButton);
-        btn.setOnClickListener(new View.OnClickListener(){
+        surfaceView = findViewById(R.id.cameraPreview);
+        textView = findViewById(R.id.textView);
+
+        detector = new BarcodeDetector.Builder(MainActivity.this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+        if(!detector.isOperational()){
+            textView.setText("Could not set up the detector!");
+            return;
+        }
+
+        cameraSource = new CameraSource.Builder(this, detector)
+                .setRequestedPreviewSize(1024, 768)
+                .setAutoFocusEnabled(true)
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
-            public void onClick(View v) {
-                ImageView myImageView = findViewById(R.id.imgview);
-                TextView txtView = findViewById(R.id.textView);
-                Bitmap myBitmap = BitmapFactory.decodeResource(
-                        getApplicationContext().getResources(),
-                        R.drawable.puppy);
-                myImageView.setImageBitmap(myBitmap);
+            public void surfaceCreated(SurfaceHolder holder) {
 
-                BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext())
-                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
-                        .build();
-                if(!detector.isOperational()){
-                    txtView.setText("Could not set up the detector!");
-                    return;
+                try {
+                    if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                        cameraSource.start(surfaceView.getHolder());
+                    }else{
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+                        cameraSource.start(surfaceView.getHolder());
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
                 }
+            }
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
-                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-                SparseArray<Barcode> barcodes = detector.detect(frame);
-
-                Barcode thisCode = barcodes.valueAt(0);
-                txtView.setText(thisCode.rawValue);
+            }
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                detector.release();
+                cameraSource.stop();
+                cameraSource.release();
             }
         });
-    }
 
-    //Called when the user taps the send button
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class); //intent provides runtime binding between separate components, e.g. two activities
-        EditText editText = findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        detector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+                Toast.makeText(getApplicationContext(), "To prevent memory leaks barcode scanner has been stopped", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if(qrCodes.size() > 0){ textView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(qrCodes.valueAt(0).displayValue);
+                    }
+                });}
+            }
+        });
     }
 }

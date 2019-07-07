6e085afbeca9feb03d4f77da6de8baec3e443f71
an95na <anna.birulina@outlook.com>
2019-07-07 14:53:47 +0200

    implement product code decoder and show scanned product

--- app/src/main/java/com/example/smartshopper/ScanDetailsActivity.java
+++ app/src/main/java/com/example/smartshopper/ScanDetailsActivity.java
@@ -2,16 +2,23 @@ package com.example.smartshopper;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.smartshopper.data.ScannedProductRepository;
 import com.example.smartshopper.data.database.Product;
 
-public class ScanDetailsActivity extends AppCompatActivity implements ScannedProductRepository.GetProductCallback {
+public class ScanDetailsActivity extends AppCompatActivity
+        implements ScannedProductRepository.GetProductCallback {
 
-    public static String EXTRA_EAN = "com.example.smartshopper.ScanDetailsActivity.ean";
+    public static final String EXTRA_EAN = "com.example.smartshopper.ScanDetailsActivity.ean";
+    private static final String TAG = "ScanDetailsActivity";
     private ScannedProductRepository scannedProductRepository;
+    private TextView scanDetailsNameValue;
+    private TextView scanResultDescriptionValue;
+    private TextView scanDetailsAllergenValue;
+    private TextView scanDetailsPackagingValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +29,33 @@ public class ScanDetailsActivity extends AppCompatActivity implements ScannedPro
 
         long ean = getIntent().getLongExtra(EXTRA_EAN, -1L);
 
-        scannedProductRepository.getProduct(ean, this);
+        scanDetailsNameValue = findViewById(R.id.scanDetailsNameValue);
+        scanResultDescriptionValue = findViewById(R.id.scanResultDescriptionValue);
+        scanDetailsAllergenValue = findViewById(R.id.scanDetailsAllergenValue);
+        scanDetailsPackagingValue = findViewById(R.id.scanDetailsPackagingValue);
 
+        scannedProductRepository.getProduct(ean, this);
     }
 
     @Override
     public void onGetProduct(Product product) {
-        Log.e("Debug", "show product ean: " + product.ean + "\ndata: " + product.data + "\nscanned: " + product.scanned);
+        Log.d(TAG, "show product ean: " + product.ean + "\ndata: " + product.data + "\nscanned: " + product.scanned);
+
+        scanDetailsNameValue.setText(product.getDetailName());
+        scanResultDescriptionValue.setText(product.getDescription());
+
+        StringBuilder allergenDescription = new StringBuilder();
+        for (String description : product.getAllergen()) {
+            allergenDescription.append(description);
+            allergenDescription.append("\n");
+        }
+        scanDetailsAllergenValue.setText(allergenDescription.toString().trim());
+
+        StringBuilder packagingDescription = new StringBuilder();
+        for (String description : product.getPackaging()) {
+            packagingDescription.append(description);
+            packagingDescription.append("\n");
+        }
+        scanDetailsPackagingValue.setText(packagingDescription.toString().trim());
     }
 }
--- app/src/main/java/com/example/smartshopper/data/database/Product.java
+++ app/src/main/java/com/example/smartshopper/data/database/Product.java
@@ -3,6 +3,10 @@ package com.example.smartshopper.data.database;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.example.smartshopper.data.util.ProductDecoder;
+
+import java.util.List;
+
 @Entity(tableName = "products")
 public class Product {
 
@@ -12,4 +16,36 @@ public class Product {
     public String data = "";
 
     public long scanned = 0L;
+
+
+    public String getName() {
+        return findValueOf("name", data.split("\\n"));
+    }
+
+    public String getDetailName() {
+        return findValueOf("detailname", data.split("\\n"));
+    }
+
+    public String getDescription() {
+        return findValueOf("descr", data.split("\\n"));
+    }
+
+    public List<String> getAllergen() {
+        String codeAsString = findValueOf("contents", data.split("\\n"));
+        return ProductDecoder.decodeAllergen(codeAsString);
+    }
+
+    public List<String> getPackaging() {
+        String codeAsString = findValueOf("pack", data.split("\\n"));
+        return ProductDecoder.decodePackaging(codeAsString);
+    }
+
+    private String findValueOf(String key, String[] source) {
+        for (String sourcePart : source) {
+            if (sourcePart.trim().startsWith(key)) {
+                return sourcePart.split("=")[1];
+            }
+        }
+        return "";
+    }
 }
